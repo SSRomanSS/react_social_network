641af751c3ca74f526e7afc8378bc62137523cd8
Roman Shkapiak <r.shkapiak@gmail.com>
2021-05-31 18:44:19 +0300

    Refactor User with class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,22 +2,48 @@ import React from "react";
 import User from "./User/User";
 const axios = require('axios')
 
-
-
-function Users(props) {
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then((data) => props.setUsers(data.data.items))
+class Users extends React.Component {
+    getUsers = () => {
+        if (this.props.users.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then((data) => this.props.setUsers(data.data.items))
+    }
     }
 
-    let UsersData = props.users.map(el => <User user={el}
-                                                followUser={props.followUser}
-                                                unfollowUser={props.unfollowUser}/>);
-    debugger
-    return (
+    UsersData = () => this.props.users.map(el => <User user={el}
+                                                followUser={this.props.followUser}
+                                                unfollowUser={this.props.unfollowUser}/>);
+
+    render() {
+        return (
         <div>
-            {UsersData}
+            <button onClick={this.getUsers}>Get Users</button>
+            {this.UsersData()}
         </div>
     )
+    }
+
+
 }
+
+// function Users(props) {
+//
+//     let getUsers = () => {
+//         if (props.users.length === 0) {
+//         axios.get('https://social-network.samuraijs.com/api/1.0/users').then((data) => props.setUsers(data.data.items))
+//     }
+//     }
+//
+//
+//     let UsersData = props.users.map(el => <User user={el}
+//                                                 followUser={props.followUser}
+//                                                 unfollowUser={props.unfollowUser}/>);
+//     debugger
+//     return (
+//         <div>
+//             <button onClick={getUsers}>Get Users</button>
+//             {UsersData}
+//         </div>
+//     )
+// }
 
 export default Users;
